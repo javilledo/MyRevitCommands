71e559ebaac180cad9a73d64d8fb653ce899b5b7
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2023-09-02 20:12:02 +0200

    Change Location added!

---
+++ MyRevitCommands/ChangeLocation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class ChangeLocation: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            // Get UIDocument and Document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+
+                // Pick Object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if (pickedObj != null)
+                {
+
+                    // Retrieve Element
+                    ElementId elementId = pickedObj.ElementId;
+                    Element element = doc.GetElement(elementId);
+
+                    using (Transaction transaction = new Transaction(doc, "Place Family"))
+                    {
+
+                        // Set Location
+                        LocationPoint locp = element.Location as LocationPoint;
+
+                        if (locp != null)
+                        {
+                            transaction.Start();
+
+                            XYZ loc = locp.Point;
+                            XYZ newloc = new XYZ(loc.X + 3, loc.Y, loc.Z);
+                            locp.Point = newloc;
+
+                            transaction.Commit();
+
+                        };
+
+                    };
+
+                }
+                
+                return Result.Succeeded;
+
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
+        }
+    }
+}
