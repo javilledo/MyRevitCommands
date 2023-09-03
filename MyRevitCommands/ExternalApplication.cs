07324e514c31ba1a89ba36a5b93f6025404eb550
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2023-09-03 21:23:39 +0200

    Ribbon and Application

---
+++ MyRevitCommands/ExternalApplication.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace MyRevitCommands
+{
+    internal class ExternalApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Create Ribbon Tab
+            application.CreateRibbonTab("My Commands");
+
+            string path = Assembly.GetExecutingAssembly().Location;
+            PushButtonData button = new PushButtonData("Button 1", "PlaceFamily", path, "MyRevitCommands.PlaceFamily");
+
+            RibbonPanel panel = application.CreateRibbonPanel("My Commands", "Commands");
+
+            // Add button image
+            Uri imagePath = new Uri(@"C:\Users\javil\source\repos\MyRevitCommands\MyRevitCommands\desk.png");
+            BitmapImage image = new BitmapImage(imagePath);
+
+            PushButton pushButton = panel.AddItem(button) as PushButton;
+            pushButton.LargeImage = image;
+
+            return Result.Succeeded;
+
+        }
+    }
+}
