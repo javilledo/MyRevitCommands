dda44c2063b55efc7b2d723467dfaa5549eed7b8
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2023-09-03 08:18:55 +0200

    Element Intersection added!

---
+++ MyRevitCommands/ElementIntersection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class ElementIntersection: IExternalCommand
+    {
+        private Solid gSolid;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            // Get UIDocument and Document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+
+                // Pick Object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if (pickedObj != null)
+                {
+
+                    // Retrieve Element
+                    ElementId elementId = pickedObj.ElementId;
+                    Element element = doc.GetElement(elementId);
+
+                    // Get Geometry
+                    Options gOptions = new Options();
+                    gOptions.DetailLevel = ViewDetailLevel.Fine;
+                    GeometryElement geom = element.get_Geometry(gOptions);
+
+                    // Traverse Geometry
+                    foreach(GeometryObject geomObj in geom)
+                    {
+
+                        GeometryInstance gInst = geomObj as GeometryInstance;
+
+                        if(gInst != null)
+                        {
+                            GeometryElement gEle = gInst.GetInstanceGeometry();
+                            foreach(GeometryObject gEleObj in gEle)
+                            {
+                                gSolid = gEleObj as Solid;
+                            };
+                        };
+
+                    };
+
+                    // Filter for Intersection
+                    FilteredElementCollector collector = new FilteredElementCollector(doc);
+                    ElementIntersectsSolidFilter filter = new ElementIntersectsSolidFilter(gSolid, false);
+                    ICollection<ElementId> intersects = collector.OfCategory(BuiltInCategory.OST_Roofs).WherePasses(filter).ToElementIds();
+
+                    uidoc.Selection.SetElementIds(intersects);
+
+                }
+                
+                return Result.Succeeded;
+
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
+        }
+    }
+}
---
+++ MyRevitCommands/ProjectRay.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class ProjectRay: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            // Get UIDocument and Document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+
+                // Pick Object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if (pickedObj != null)
+                {
+
+                    // Retrieve Element
+                    ElementId elementId = pickedObj.ElementId;
+                    Element element = doc.GetElement(elementId);
+
+                    // Project Ray
+                    LocationPoint locP = element.Location as LocationPoint;
+                    XYZ p1 = locP.Point;
+
+                }
+                
+                return Result.Succeeded;
+
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
+        }
+    }
+}
